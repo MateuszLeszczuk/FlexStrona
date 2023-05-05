102a908ec5b7b8ac63ab5d9409782df02b7c4160
Mateusz <leszczukpriv@gmail.com>
2023-05-05 14:15:22 +0200

    added logic to hamburger menu, logic in footer year

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const nav = document.querySelector('.nav');
 const navBtn = document.querySelector('.burger-btn');
 const allNavItems = document.querySelectorAll('.nav__item');
-
+const navBtnBars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
 
 
 const handleNav = () => {
@@ -20,9 +22,33 @@ const handleNavItemsAnimation = () => {
 
     allNavItems.forEach(item => {
         item.classList.toggle('nav-items-animation');
-         item.style.animationDelay = '.' + delayTime + 's';
-         delayTime++;
+        item.style.animationDelay = '.' + delayTime + 's';
+        delayTime++;
     })
 }
 
+const handleObserver = () =>{
+    const currentSection = window.scrollY;
+
+    allSections.forEach(section => {
+
+        if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.add('black-bars-color');
+            
+        } else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60){
+            navBtnBars.classList.remove('black-bars-color');
+            
+        }
+    })
+}
+
+const hadnleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
+
+
+
+hadnleCurrentYear();
 navBtn.addEventListener('click', handleNav);
+window.addEventListener('scroll',handleObserver);
